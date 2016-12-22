9862e8b0c51fc7f54fbbb0fa0bad6693fef6165d
cpsdqs <creeper32605@gmail.com>
2016-12-22 12:39:04 +0100

    Make it use a dark theme

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ anagram
 =======
 lets you quickly check for anagrams
 
-![That is a sentence and also another anagram | Some sane things eat a Han or a neat dart clan](https://i.imgur.com/CmWaMrB.png)
-![This is, in fact, invalid | a hand finds cans](https://i.imgur.com/8QHC3Of.png)
+![That is a sentence and also another anagram | Some sane things eat a Han or a neat dart clan](https://i.imgur.com/XlQ4N7N.png)
+
+
+![Invalid | Anagram](https://i.imgur.com/1PfEWgm.png)
--- index.js
+++ index.js
@@ -1,151 +1,151 @@
-'use strict';
+let dispHeight = 150
+let dispScale = 1
 
-let countChars = function countChars(inp) {
-	let chars = {};
-	for (let char of inp.toUpperCase()) {
-		if (char.match(/^\W$/)) continue;
-		if (!(char in chars)) {
-			chars[char] = 1;
-		} else {
-			chars[char]++;
-		}
-	}
-	return chars;
-};
-
-let getCountTable = function getCountTable(a, b) {
-	let keys = [];
-	let table = {};
-	for (let i in a) {
-		if (!keys.includes(i)) keys.push(i);
-	}
-	for (let i in b) {
-		if (!keys.includes(i)) keys.push(i);
-	}
-	keys.sort(function(a, b) {
-		if (a.codePointAt(0) > b.codePointAt(0)) {
-			return 1;
-		} else if (a.codePointAt(0) < b.codePointAt(0)) {
-			return -1;
-		}
-		return 0;
-	})
-	for (let i of keys) {
-		table[i] = [(a[i] || 0), (b[i] || 0)];
-	}
-	return table;
+let countChars = function countChars (inp) {
+  let chars = {}
+  for (let char of inp.toUpperCase()) {
+    if (char.match(/^\W$/)) continue
+    if (!(char in chars)) {
+      chars[char] = 1
+    } else {
+      chars[char]++
+    }
+  }
+  return chars
 }
-let cTableMatches = function cTableMatches(t) {
-	let match = true;
-	for (let i in t) {
-		if (t[i][0] != t[i][1]) {
-			match = false;
-		}
-	}
-	return match;
-};
 
-let inp1 = document.querySelector('#ana1');
-let inp2 = document.querySelector('#ana2');
-let stat = document.querySelector('#status');
-let errt = document.querySelector('#err-table');
-let canv = document.querySelector('#an-canvas');
+let getCountTable = function getCountTable (a, b) {
+  let keys = []
+  let table = {}
+  for (let i in a) {
+    if (!keys.includes(i)) keys.push(i)
+  }
+  for (let i in b) {
+    if (!keys.includes(i)) keys.push(i)
+  }
+  keys.sort((a, b) => {
+    if (a.codePointAt(0) > b.codePointAt(0)) return 1
+    else if (a.codePointAt(0) < b.codePointAt(0)) return -1
+    return 0
+  })
+  for (let i of keys) {
+    table[i] = [(a[i] || 0), (b[i] || 0)]
+  }
+  return table
+}
+let cTableMatches = function cTableMatches (t) {
+  let match = true
+  for (let i in t) {
+    if (t[i][0] !== t[i][1]) {
+      match = false
+    }
+  }
+  return match
+}
+
+let inp1 = document.querySelector('#ana1')
+let inp2 = document.querySelector('#ana2')
+let stat = document.querySelector('#status')
+let errt = document.querySelector('#err-table')
+let canv = document.querySelector('#an-canvas')
 
 Object.defineProperty(window, 'dp', {
-	enumerable: true,
-	get: function() {
-		return window.devicePixelRatio;
-	}
-});
-let checkValidity;
-let resizeCanvas = function() {
-	canv.width = parseInt(getComputedStyle(canv).width) * dp;
-	canv.height = parseInt(getComputedStyle(canv).height) * dp;
-	checkValidity();
-};
-let ctx = canv.getContext('2d');
-window.addEventListener('resize', resizeCanvas);
+  enumerable: true,
+  get: function () {
+    return window.devicePixelRatio * dispScale
+  }
+})
+let checkValidity
+let resizeCanvas = function () {
+  let rect = canv.getBoundingClientRect()
+  canv.width = rect.width * window.dp
+  canv.height = rect.height * window.dp
+  checkValidity()
+}
+let ctx = canv.getContext('2d')
+window.addEventListener('resize', resizeCanvas)
 
-let updateCanvas = function() {
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	ctx.fillStyle = '#000000';
-	ctx.font = (dp * 16) + 'px monospace';
-	ctx.textAlign = 'center';
-	let height = Math.min(canv.height, 150 * dp);
-	let index = 0;
-	let chars = {};
-	for (let i of inp1.value) {
-		ctx.fillText(i, (7 + index * 12) * dp, dp * 16);
-		if (!(i in chars)) chars[i.toLowerCase()] = [index];
-		else chars[i.toLowerCase()].push(index);
-		index++;
-	}
-	index = 0;
-	ctx.lineWidth = 2 * dp;
-	ctx.lineCap = 'round';
-	ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
-	for (let i of inp2.value) {
-		ctx.fillText(i, (7 + index * 12) * dp, height - (dp * 5));
-		if (!i.match(/^\W$/) && chars[i.toLowerCase()] && chars[i.toLowerCase()].length) {
-			let ci = chars[i.toLowerCase()][0];
-			chars[i.toLowerCase()].splice(0, 1);
-			ctx.beginPath();
-			let x1 = (7 + index * 12) * dp;
-			let y1 = height - (dp * 20);
-			let x2 = (7 + ci * 12) * dp;
-			let y2 = dp * 18;
-			ctx.moveTo(x1, y1);
-			ctx.bezierCurveTo(x1, height * 0.7, x2, height * 0.3, x2, y2);
-			ctx.stroke();
-		}
-		index++;
-	}
-};
+let updateCanvas = function () {
+  let dp = window.dp
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  ctx.fillStyle = '#fafafa'
+  ctx.font = (dp * 16) + 'px Fira Mono, Menlo, Monaco, Inconsolata, Consolas, Lucida Console, monospace'
+  ctx.textAlign = 'center'
+  let height = Math.min(canv.height, dispHeight * dp)
+  let index = 0
+  let chars = {}
+  for (let i of inp1.value) {
+    ctx.fillText(i, (7 + index * 12) * dp, dp * 16)
+    if (!(i in chars)) chars[i.toLowerCase()] = [index]
+    else chars[i.toLowerCase()].push(index)
+    index++
+  }
+  index = 0
+  ctx.lineWidth = 2 * dp
+  ctx.lineCap = 'round'
+  ctx.strokeStyle = 'rgba(255, 255, 255, 0.5)'
+  for (let i of inp2.value) {
+    ctx.fillText(i, (7 + index * 12) * dp, height - (dp * 5))
+    if (!i.match(/^\W$/) && chars[i.toLowerCase()] && chars[i.toLowerCase()].length) {
+      let ci = chars[i.toLowerCase()][0]
+      chars[i.toLowerCase()].splice(0, 1)
+      ctx.beginPath()
+      let x1 = (7 + index * 12) * dp
+      let y1 = height - (dp * 20)
+      let x2 = (7 + ci * 12) * dp
+      let y2 = dp * 18
+      ctx.moveTo(x1, y1)
+      ctx.bezierCurveTo(x1, height * 0.7, x2, height * 0.3, x2, y2)
+      ctx.stroke()
+    }
+    index++
+  }
+}
 
-checkValidity = function checkValidity() {
-	let i1 = countChars(inp1.value);
-	let i2 = countChars(inp2.value);
-	errt.innerHTML = '';
-	let tab = getCountTable(i1, i2);
-	if (cTableMatches(tab)) {
-		stat.textContent = 'Valid Anagram';
-		stat.setAttribute('valid', true);
-	} else {
-		stat.textContent = 'Invalid Anagram';
-		stat.setAttribute('valid', false);
-		let h0 = document.createElement('th');
-		let h1 = document.createElement('th');
-		let h2 = document.createElement('th');
-		h0.textContent = '';
-		h1.textContent = 'Input 1';
-		h2.textContent = 'Input 2';
-		let th = document.createElement('tr');
-		th.appendChild(h0);
-		th.appendChild(h1);
-		th.appendChild(h2);
-		errt.appendChild(th);
-		for (let i in tab) {
-			let j = tab[i];
-			let q0 = document.createElement('td');
-			let q1 = document.createElement('td');
-			let q2 = document.createElement('td');
-			q0.textContent = i;
-			q1.textContent = j[0];
-			q2.textContent = j[1];
-			let tr = document.createElement('tr');
-			tr.appendChild(q0);
-			tr.appendChild(q1);
-			tr.appendChild(q2);
-			if (j[0] == j[1]) tr.classList.add('match');
-			else if (j[0] < j[1]) tr.classList.add('diffr')
-			else tr.classList.add('diffl')
-			errt.appendChild(tr);
-		}
-	}
-	updateCanvas();
-};
-resizeCanvas();
-resizeCanvas();
+checkValidity = function checkValidity () {
+  let i1 = countChars(inp1.value)
+  let i2 = countChars(inp2.value)
+  errt.innerHTML = ''
+  let tab = getCountTable(i1, i2)
+  if (cTableMatches(tab)) {
+    stat.textContent = 'Valid Anagram'
+    stat.setAttribute('valid', true)
+  } else {
+    stat.textContent = 'Invalid Anagram'
+    stat.setAttribute('valid', false)
+    let h0 = document.createElement('th')
+    let h1 = document.createElement('th')
+    let h2 = document.createElement('th')
+    h0.textContent = ''
+    h1.textContent = 'Input 1'
+    h2.textContent = 'Input 2'
+    let th = document.createElement('tr')
+    th.appendChild(h0)
+    th.appendChild(h1)
+    th.appendChild(h2)
+    errt.appendChild(th)
+    for (let i in tab) {
+      let j = tab[i]
+      let q0 = document.createElement('td')
+      let q1 = document.createElement('td')
+      let q2 = document.createElement('td')
+      q0.textContent = i
+      q1.textContent = j[0]
+      q2.textContent = j[1]
+      let tr = document.createElement('tr')
+      tr.appendChild(q0)
+      tr.appendChild(q1)
+      tr.appendChild(q2)
+      if (j[0] === j[1]) tr.classList.add('match')
+      else if (j[0] < j[1]) tr.classList.add('diffr')
+      else tr.classList.add('diffl')
+      errt.appendChild(tr)
+    }
+  }
+  updateCanvas()
+}
+resizeCanvas()
+resizeCanvas()
 
-inp1.addEventListener('keyup', checkValidity);
-inp2.addEventListener('keyup', checkValidity);
+inp1.addEventListener('keyup', checkValidity)
+inp2.addEventListener('keyup', checkValidity)
